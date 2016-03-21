e476dce04ed3f49b4cc18bfe76a5c5466ef41533
Oren Freifeld <freifeld@csail.mit.edu>
2016-03-21 10:39:40 -0400

    cpaNd/decide_sharedmemory.py

--- cpab/cpaNd/decide_sharedmemory.py
+++ cpab/cpaNd/decide_sharedmemory.py
@@ -45,7 +45,9 @@ def decide_sharedmemory(dim_domain,dim_range,nC):
         if computer.has_good_gpu_card==0 and  nC <= 4**4: 
             sharedmemory=2  
         elif computer.has_good_gpu_card==0 and  nC <= 4**5: 
-            sharedmemory=1  
+            sharedmemory=1 
+        elif computer.has_good_gpu_card and nC <= 4**5:
+            sharedmemory=2
         else:
             raise NotImplementedError(computer.has_good_gpu_card, dim_domain,  nC)
     else:
